6289ba6c8b944f93a56ace8add44a6df5e8d6189
Iacopo Carlini <iacopocarlini.mail@gmail.com>
2020-07-28 19:42:09 +0200

    Update RoomListItem.tsx

--- src/components/RoomListItem.tsx
+++ src/components/RoomListItem.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import {
-  IonItem,
-  IonLabel,
-  IonNote
-  } from '@ionic/react';
-import './MessageListItem.css';
+import {IonItem, IonLabel, IonNote, IonBadge, IonButton, IonIcon} from '@ionic/react';
+import './RoomListItem.css';
+import { closeOutline } from 'ionicons/icons';
+import *  as re from '../script.js';
 
-const MessageListItem: React.FC<any> = ({ room }) =>
+const RoomListItem: React.FC<any> = ({ room, newMessages }) =>
 {
   return (
-    <IonItem routerLink={`/room/${room.roomId}`} detail={false}>
-      <div slot="start" className="dot dot-unread"></div>
+    <IonItem routerLink = {`/room/${room.roomId}`} detail={false}>
+      <div slot="start" className = "dot dot-unread"></div>
       <IonLabel className="ion-text-wrap">
         <h2>
-          {room.name}
+          { room.name }
           <span className="date">
-            <IonNote>DATA</IonNote>
+            <IonNote> <IonBadge color="primary"> { newMessages }</IonBadge> </IonNote>
+            <IonNote>
+              <IonButton size="small" color="light" onClick = {() => re.deleteRoom(room)} >
+                <IonIcon icon={closeOutline} />
+              </IonButton>
+          </IonNote>
           </span>
         </h2>
           <h3>Members: {room.getJoinedMembers().length}</h3>
-        <p>
-          PREVIEW
-        </p>
       </IonLabel>
-    </IonItem>
+      </IonItem>
   );
 };
 
-export default MessageListItem;
+export default RoomListItem;
+// rendering condizionale per il badge (solo se è +1 o superiore)
