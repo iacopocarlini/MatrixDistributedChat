5ef8560d527e9d2277af85bf9ea89889fd278c50
Iacopo Carlini <iacopocarlini.mail@gmail.com>
2020-07-28 19:43:23 +0200

    Update MessageListItem.tsx

--- src/components/MessageListItem.tsx
+++ src/components/MessageListItem.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import {
-  IonItem,
-  IonLabel,
-  IonNote
-  } from '@ionic/react';
+import {IonItem, IonLabel, IonNote} from '@ionic/react';
 import './MessageListItem.css';
+
+function getMessageTime(message)
+{
+  var d = new Date(message.event.origin_server_ts);
+  return d.getHours().toString() + ':' + d.getMinutes().toString() + ' (' + d.getDate().toString() + '/' + (d.getMonth()+1).toString() + ')'; 
+}
 
 const MessageListItem: React.FC<any> = ({ message }) =>
 {
+
   return (
     <IonItem>
-      <div slot="start" className="dot dot-unread"></div>
-      <IonLabel className="ion-text-wrap">
-        <h2>
-         {message.sender.name}
-          <span className="date">
-            <IonNote>{message.event.type}</IonNote>
-          </span>
-        </h2>
-          <h3>{message.event.content.body}</h3>
-      </IonLabel>
-    </IonItem>
+    {message.getType() === "m.room.message" ?
+        (
+        <>
+          <div slot="start" className="dot dot-unread"></div>
+            <IonLabel className="ion-text-wrap">
+              <h2>
+              {message.sender.name}
+                <span className="date">
+                  <IonNote>{ getMessageTime(message)  }</IonNote>
+                </span>
+              </h2>
+                <h3>{message.event.content.body}</h3>
+            </IonLabel>
+          </>
+        ) 
+        : 
+        <p></p>}
+      </IonItem>
   );
 };
 
