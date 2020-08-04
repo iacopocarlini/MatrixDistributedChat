0ea2f669a5abc15abb9ba02fa7cc130629a3199b
Iacopo Carlini <iacopocarlini.mail@gmail.com>
2020-08-04 12:44:05 +0200

    Add files via upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,82 @@
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonBadge, useIonViewWillEnter} from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, useIonViewWillEnter} from '@ionic/react';
 import { add } from 'ionicons/icons';
 import{IonList, IonRefresher, IonRefresherContent} from '@ionic/react';
 import './Home.css';
-
 import *  as re from '../script.js';
 import RoomListItem from '../components/RoomListItem';
 
 const Home: React.FC = () =>
 {
+  // State variables / Hooks
   const [rooms, refreshRooms] = useState<any[]>([]);
+  const [notifications, setNotifications] = useState({});
 
-  re.matrixClient.once('sync', function(state, prevState, res) // client sync
+  // Client sync with server
+  re.matrixClient.once('sync', function(state, prevState, res)
   {
+    /* Managing state
+    if(state === 'PREPARED')
+    {
+      console.log("prepared");
+      refreshRooms(re.matrixClient.getRooms());
+    } 
+    else {
+        console.log(state);
+        //process.exit(1);
+    }
+    */
+    
     refreshRooms(re.matrixClient.getRooms());
   });
+  
+  const userID = re.matrixClient.getUserId();
 
+  // View in...
   useIonViewWillEnter(() =>
   {
-    refreshRooms(re.matrixClient.getRooms());
+    
+    // Notifications check
+    re.matrixClient.getRooms().forEach((room) =>
+    {
+      if (room.getUnreadNotificationCount("total") > 0)
+        notifications[room.roomId] = true;
+      else
+        notifications[room.roomId] = false;
+    });
+    setNotifications(notifications);
+
+
+    // Rooms
+    refreshRooms(re.matrixClient.getRooms()); // restoring rooms state
   });
 
-  const userID = re.matrixClient.credentials.userId;
+  // # CLIENT LOGIC
 
-  // Automatically join rooms when invited
+  // ## MEMBERSHIP LOGIC : Automatically join rooms when invited
   re.matrixClient.on("RoomMember.membership", function(event, member) 
   {
     if (member.membership === "invite" && member.userId === userID)
-    {
-      re.matrixClient.joinRoom(member.roomId).then(function(){
-        });
-    }
+      re.matrixClient.joinRoom(member.roomId);
   });
+
+  // ## Room events listener
+  re.matrixClient.on("Room.timeline", function(event, room, toStartOfTimeline)
+  {
+    if (event.getType() === "m.room.message") // triggered for any message from any room...
+    {
+      if (room.getUnreadNotificationCount("total") > 0)
+        notifications[room.roomId] = true;
+      else
+        notifications[room.roomId] = false;
+
+      setNotifications(notifications);
+    }
+
+    else // not a message
+      return;
+  });
+  
 
   const refresh = (e: CustomEvent) => 
   {
@@ -41,28 +86,23 @@ const Home: React.FC = () =>
   };
 
   return (
+
     <IonPage id="home-page">
+
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Rooms</IonTitle>
+          <IonTitle> Rooms </IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
         <IonRefresher slot="fixed" onIonRefresh={refresh}>
           <IonRefresherContent></IonRefresherContent>
         </IonRefresher>
 
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">
-              Inbox
-            </IonTitle>
-          </IonToolbar>
-        </IonHeader>
-
         <IonList>
         {
-          rooms.map(roomElement => <RoomListItem key = {roomElement.roomId} room = {roomElement} newMessages = {1}/>)          
+          rooms.map(roomElement => <RoomListItem key = {roomElement.roomId} room = {roomElement} newMessages = {notifications[roomElement.roomId]}/>)          
         }
         </IonList>
         
@@ -73,6 +113,7 @@ const Home: React.FC = () =>
         </IonFab>
       </IonContent>
     </IonPage>
+
   );
 };
 
--- src/pages/ViewRoom.tsx
+++ src/pages/ViewRoom.tsx
@@ -1,50 +1,115 @@
-import React, { useState } from 'react';
-import {IonBackButton,IonButtons,IonContent,IonHeader,IonList,IonPage,IonToolbar,IonItem,IonInput,IonFab,IonFabButton,IonIcon,useIonViewWillEnter} from '@ionic/react';
+import React, { useState, useEffect } from 'react';
+import {IonBackButton, IonButtons, IonButton, IonContent, IonSelect, IonSelectOption, IonHeader, IonTitle, IonList, IonPage, IonToolbar, IonItem, IonInput, IonFab, IonFabButton, IonIcon, useIonViewDidEnter, useIonViewWillEnter} from '@ionic/react';
 import MessageListItem from '../components/MessageListItem';
 import './ViewRoom.css';
 import *  as re from '../script.js';
 import { chevronForwardOutline } from 'ionicons/icons';
+import { getFriends, Friend } from '../data/friend';
 
-const ViewMessage: React.FC<any> = ({ match }) => 
+const ViewRoom: React.FC<any> = ({ match }) => 
 {
+  const roomID = match.params.id;
+
+  // Hooks
   const [messages, setMessages] = useState<any[]>([]); // array di events
   const [text, setText] = useState<string>();
-  const roomID = match.params.id;
+  const [inviteList, setInviteList] = useState<Friend[]>([]);
+  const [userToInvite, setUserToInvite] = useState("");
+  const [loadingFriends, setLoadingFriends] = useState(true);
   
+
+  // Room event listener
   re.matrixClient.on("Room.timeline", function(event, room, toStartOfTimeline)
   {
     if (event.getType() === "m.room.message" && event.getRoomId() === roomID) // condizioni messaggio
-    {
       setMessages([...messages, event]);
-    }
+
   });
 
+
   // View in...
-  useIonViewWillEnter(() => 
+  useIonViewWillEnter(() =>
   {
+    // Chat is read...
+    if (re.matrixClient.getRoom(roomID) !== null)
+    {
+      re.matrixClient.getRoom(roomID).setUnreadNotificationCount("total", 0);
+      re.matrixClient.getRoom(roomID).setUnreadNotificationCount("highlight", 0);
+    }
+
+    // Retrieve messages...
     Object.keys(re.matrixClient.store.rooms).forEach((roomId) =>
     {
       if (roomId === roomID)
-      {
         setMessages(re.matrixClient.getRoom(roomId).timeline);
-      }
     });
   });
 
+  
+  // Update friends list
+  useEffect(() =>
+  {
+    if (loadingFriends)
+    {
+      // Retrieve friends list
+      var members = re.matrixClient.getRoom(roomID).getJoinedMembers();
+      var friends = getFriends();
+    
+      friends.forEach(f => {
+        var alreadyIN = false;
+        members.forEach(m => {
+          if (f.id === m.name)
+            alreadyIN = true;
+        });
+        if (!alreadyIN)
+          inviteList.push(f);
+      });
+
+      setInviteList(inviteList);
+      setLoadingFriends(false);
+    }
+  });
+  
+
   const sendMessageAction = () => 
   {
-    // event.preventDefault(); // evita refresh?
     re.sendMessage(text, match.params.id);
     setText('');
   }
 
+  const inviteUserAction = () => 
+  {
+    if (userToInvite !== null && userToInvite !== "")
+    {
+      re.inviteUser(userToInvite, roomID);
+      setUserToInvite('');
+    }
+    else
+      return;
+  }
+
   return (
-    <IonPage id="view-message-page">
+    <IonPage id="view-room-page">
       <IonHeader translucent>
         <IonToolbar>
-          <IonButtons>
+
+          <IonButtons slot="start">
             <IonBackButton text="Rooms" defaultHref="/home"></IonBackButton>
           </IonButtons>
+
+          <IonButton slot="end" onClick = {inviteUserAction}>Invite</IonButton>
+            <IonSelect slot="end" value={userToInvite} okText="Okay" cancelText="Cancel" onIonChange={e => setUserToInvite(e.detail.value)}>
+            {
+              inviteList.map(friend => <IonSelectOption key = {friend.id} value = {friend.id}> {friend.id} </IonSelectOption>)
+            }
+          </IonSelect>
+
+          <IonButtons slot="end">
+            <IonButton color="primary" onClick = {() => re.leaveRoom(roomID)} href = "/home">Leave room</IonButton>
+          </IonButtons>
+
+        <IonTitle>{ match.params.name }</IonTitle>
+
         </IonToolbar>
       </IonHeader>
 
@@ -53,9 +118,9 @@ const ViewMessage: React.FC<any> = ({ match }) =>
         (
         <>
         <IonList>
-            {
-              messages.map(m => <MessageListItem key = {m.event.event_id} message = {m}/>)          
-            }
+        {
+          messages.map(m => <MessageListItem key = {m.event.event_id} message = {m}/>)          
+        }
         </IonList>
             
         <IonItem>
@@ -68,10 +133,10 @@ const ViewMessage: React.FC<any> = ({ match }) =>
         </IonItem>
         
         </>
-        ) : <div>Message not found</div>}
+        ) : <div>Couldn't retrieve chat</div>}
       </IonContent>
     </IonPage>
   );
 };
 
-export default ViewMessage; 
+export default ViewRoom;
